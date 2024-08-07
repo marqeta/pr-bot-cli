2eb646213e39d3ae386794879628c1c7a2ae3e36
Xueying Mao <xmao@marqeta.com>
2024-08-07 13:01:57 -0700

    update workflow

--- cmd/pr-bot-cli/main.go
+++ cmd/pr-bot-cli/main.go
@@ -95,7 +95,7 @@ func evaluatePullRequest(cmd *cobra.Command, args []string) {
 
 func setupGHEClients() (*github.Client, *githubv4.Client) {
 	log.Info().Msg("Setting up GHE clients")
-	tok := os.Getenv("INPUT_GITHUB_TOKEN")
+	tok := os.Getenv("GITHUB_TOKEN")
 	if tok == "" {
 		log.Error().Msg("GITHUB_TOKEN not set")
 		os.Exit(1)
